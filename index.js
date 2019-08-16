b1078425d4ba72feae7f62c4c0d3e7b6bcb9ce15
sweyome <sweyome1@gmail.com>
2019-08-16 00:06:13 -0400

    config for deployment

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
+const port = process.env.PORT || 4000;
 
 server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+  console.log(`\n*** Server Running on ${port} ***\n`);
 });
+//! when running locally the IP is taking care of my localHost!!!
+//! and we can hard code any old port(over 3000) in
+
+
+//! when deploying
+//! heroku will assign an Ip address
+//! we can't hard code in the port
+//! heroku needs to tell us which port
+//! (because it might be taken) 
